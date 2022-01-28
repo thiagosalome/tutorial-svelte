0fc25aee6070f1b6a5be1ff7b5655fe7d96e2725
Thiago Salome <thiagosalome@gmail.com>
2022-01-28 07:06:37 -0300

    Logic / Await blocks

--- src/App.svelte
+++ src/App.svelte
@@ -67,6 +67,42 @@
 		{ id: 'z_AbfPXTKms', name: 'Maru' },
 		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
 	];
+
+	// -------------------------------------------------------
+
+	import Thing from './components/Thing.svelte'
+
+	let things = [
+		{ id: 1, name: 'apple' },
+		{ id: 2, name: 'banana' },
+		{ id: 3, name: 'carrot' },
+		{ id: 4, name: 'doughnut' },
+		{ id: 5, name: 'egg' },
+	];
+
+	function handleClick () {
+		things = things.slice(1)
+	}
+
+	// -------------------------------------------------------
+
+	async function getImages() {
+		const response = await fetch('https://fakerapi.it/api/v1/images?_width=380&_quantity=5')
+		const data = await response.json()
+
+		if (response.ok) {
+			return data.data
+		} else {
+			throw new Error(data)
+		}
+	}
+
+	let promiseImages = getImages()
+
+	function handleClickGenerateImages () {
+		promiseImages = getImages()
+	}
+
 </script>
 
 <main>
@@ -125,6 +161,36 @@
 			</li>
 		{/each}
 	</ul>
+
+	<hr>
+
+	<button on:click={handleClick}>
+		Remove first thing
+	</button>
+	
+	{#each things as thing (thing.id)}
+		<Thing name={thing.name} />
+	{/each}
+
+	<hr>
+
+	<button on:click={handleClickGenerateImages}>Generate Images</button>
+
+	{#await promiseImages}
+		<p>Carregando imagens...</p>
+	{:then data}
+		<div class="articles">
+			{#each data as dataItem (dataItem.title)}
+				<div>
+					<img src={dataItem.url} title={dataItem.title} alt={dataItem.title}>
+					<h3>{dataItem.title}</h3>
+					<p>{dataItem.description}</p>
+				</div>
+			{/each}
+		</div>
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
 </main>
 
 <style>
@@ -140,6 +206,16 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	.articles {
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+		grid-gap: 1rem;
+	}
+
+	.articles img {
+		max-width: 100%;
 	}
 
 	@media (min-width: 640px) {
